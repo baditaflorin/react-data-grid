525908639c278dbb282b07d68f923058b050692f
Florin Badita-Nistor <baditaflorin@gmail.com>
2024-04-16 09:07:58 +0000

    get data from object

--- website/demos/components/columns/LatLonButton.tsx
+++ website/demos/components/columns/LatLonButton.tsx
@@ -2,11 +2,13 @@ export function LatLonButton({ country, onUpdate }) {
   const fetchLatLon = async () => {
     try {
       const response = await fetch(
-        `https://geo.geocode.okssh.com/?text=${encodeURIComponent(country)}&final=true&output=json`
+        `${import.meta.env.VITE_REACT_DB_LATLON_SEARCH_URL}${encodeURIComponent(country)}`
       );
       const data = await response.json();
-      if (data && data.length > 0) {
-        const { latitude, longitude } = data[0];
+
+      // Change this check to ensure data is not null and has latitude property
+      if (data && data.latitude !== undefined) {
+        const { latitude, longitude } = data;
         onUpdate(`${latitude}, ${longitude}`);
       } else {
         console.error('No results found for the specified country.');
